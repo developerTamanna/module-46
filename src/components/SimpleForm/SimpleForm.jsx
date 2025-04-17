513faad20a5045cf9947c8bc3b1b6d7869d198d6
developerTamanna <astamanna57@gmail.com>
2025-04-17 14:43:19 +0600

    valu collect by form

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 const SimpleForm = () => {
+
+     const handleSubmit =(event)=>{
+        event.preventDefault()
+        console.log( event.target.text.value)
+        console.log(event.target.email.value)
+     }
+
+
     return (
-        <div>
-            <h1>simpleForm</h1>
+        <div className='mt-10'>
+            <form onSubmit={handleSubmit}>
+            <input className='border border-blue-400 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500' type="text" name='text' placeholder='type....'/>
+
+            <br/>
+            <br/>
+          
+             <br/>
+             <br/>
+             <input className='border  border-blue-400 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500' type="email" name="email" id="" placeholder='type email'/>
+
+             <br/>
+             <br/>
+             <input className='border  border-blue-400 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500' type="submit" value="submit"/>
+            </form>
         </div>
     );
 };

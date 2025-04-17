8e85cd12117ed98a87e9f24520bc6d3f441a72a5
developerTamanna <astamanna57@gmail.com>
2025-04-17 13:58:09 +0600

    file create

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+
+
+  return (
+   <div>
+   <h1 className='text-3xl'>Explore react form</h1>
+
+   <div>
+
+    <SimpleForm></SimpleForm>
+   </div>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SimpleForm = () => {
+    return (
+        <div>
+            <h1>simpleForm</h1>
+        </div>
+    );
+};
+
+export default SimpleForm;
